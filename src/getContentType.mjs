0c17a2d23aabfd10170968da7d174687dcbba89d
Jascha <jascha@jaeh.at>
2020-01-15 17:55:39 +0100

    format

--- src/getContentType.mjs
+++ src/getContentType.mjs
@@ -2,7 +2,7 @@ import { getFileType } from './getFileType.mjs'
 import { contentTypes } from './contentTypes.mjs'
 
 import is from '@magic/types'
-import error from  '@magic/error'
+import error from '@magic/error'
 
 const libName = '@magic/fs.getContentType'
 
@@ -10,6 +10,7 @@ export const getContentType = uri => {
   if (is.empty(uri)) {
     throw error(`${libName}: need uri to be a non-empty string`, 'E_ARG_EMPTY')
   }
+
   if (!is.string(uri)) {
     throw error(`${libName}: need uri to be a string`, 'E_ARG_TYPE')
   }

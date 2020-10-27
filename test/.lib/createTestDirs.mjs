354e1375cfd83778c2802c49f0fc7402e09c65e5
jascha <jascha@jaeh.at>
2020-10-27 22:44:36 +0100

    test/.lib/createTestDirs.mjs: create deeper dir structure

--- test/.lib/createTestDirs.mjs
+++ test/.lib/createTestDirs.mjs
@@ -13,14 +13,17 @@ export const createTestDirs = id => async () => {
   const files = [
     path.join(dir, 'test', 'deep', 'test.js'),
     path.join(dir, 'test2', 'deep', 'test2.js'),
+    path.join(dir, 'test2', 'deep', 'deeper', 'deep.js'),
     path.join(dir, 'test.js'),
   ]
 
   await fs.mkdirp(path.join(dir, 'test', 'deep'))
   await fs.mkdirp(path.join(dir, 'test2', 'deep'))
+  await fs.mkdirp(path.join(dir, 'test2', 'deep', 'deeper'))
 
   await Promise.all(files.map(async f => await fs.writeFile(f, 't')))
 
+  // delete files after test has run
   return async () => {
     await fs.rmrf(dir)
   }

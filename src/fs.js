e43cf56e9406e151453a1c61321d855caaf831ed
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-06-22 00:24:54 +0200

    refactor(fs): use fs.access for exists, remove util.promisify

--- src/fs.js
+++ src/fs.js
@@ -1,19 +1,24 @@
-import fso from 'node:fs'
-import util from 'node:util'
-
-const readDir = fso.promises.readdir
-const readFile = fso.promises.readFile
-const rmdir = fso.promises.rmdir
+import fso, { constants } from 'node:fs'
+import { access } from 'node:fs/promises'
 
 export const fs = /** @type {const} */ ({
   ...fso,
   ...fso.promises,
-  exists: util.promisify(fso.exists),
-  readdir: readDir,
-  readDir,
-  readFile,
-  readfile: readFile,
-  rmdir,
-  rmDir: rmdir,
+  exists: async (/** @type {import('node:fs').PathLike} */ f) => {
+    try {
+      await access(f, constants.F_OK)
+      return true
+    } catch {
+      return false
+    }
+  },
+  readdir: fso.promises.readdir,
+  readDir: fso.promises.readdir,
+  readFile: fso.promises.readFile,
+  readfile: fso.promises.readFile,
+  rmdir: fso.promises.rmdir,
+  rmDir: fso.promises.rmdir,
   watch: fso.watch,
+  access,
+  constants,
 })
